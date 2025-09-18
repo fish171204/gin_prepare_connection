5a917782a3096577c251ecb131d08ba413a416ad
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-18 20:23:42 +0700

    feat(handler): add boilerplate handler and router with Gin

--- internal/handlers/users_handler.go
+++ internal/handlers/users_handler.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+}
+
+func NewUserHandler() *UserHandler {
+	return &UserHandler{}
+}
+
+func (uh *UserHandler) GetUserByUuid(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"data": "Get user by uuid"})
+}
+
+func (uh *UserHandler) CreateUser(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"data": "Create user"})
+}
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"hoc-gin/internal/handlers"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -11,4 +13,11 @@ func main() {
 	if err != nil {
 		log.Println("No .env file found")
 	}
+
+	r := gin.Default()
+
+	userHandler := handlers.NewUserHandler()
+	r.GET("/api/v1/users/:id", userHandler.GetUserByUuid)
+	r.POST("/api/v1/users/", userHandler.CreateUser)
+
 }
