5c6fbcd155ed8705af9526b5d0da6ea82e3a204e
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-21 11:13:54 +0700

    feat(db): verify safe connection with PingContext and timeout

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -1,9 +1,12 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"hoc-gin/internal/config"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -20,6 +23,21 @@ func InitDB() error {
 		log.Fatal("unable to use data source name", err)
 	}
 	defer DB.Close()
+
+	DB.SetMaxIdleConns(3)                   // Số kết nối nhàn rỗi tối đa
+	DB.SetMaxOpenConns(3)                   // Số kết nối tối đa
+	DB.SetConnMaxLifetime(30 * time.Minute) // Đóng kết nối sau 30p
+	DB.SetConnMaxIdleTime(5 * time.Minute)  // Đóng kết nối nhàn rỗi sau 5p
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := DB.PingContext(ctx); err != nil {
+		DB.Close()
+		return fmt.Errorf("DB ping error: %w", err)
+	}
+
+	log.Println("Connected")
 
 	return nil
 }
