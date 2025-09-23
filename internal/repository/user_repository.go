a02b47364f629705ce52bfd7bdc09e26de1ccdad
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-23 15:08:59 +0700

    complete main

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,15 +1,19 @@
 package repository
 
 import (
+	"hoc-gin/internal/db/sqlc"
 	"hoc-gin/internal/models"
 	"log"
 )
 
 type SQLUserRepository struct {
+	db *sqlc.Queries
 }
 
-func NewSQLUserRepository() UserRepository {
-	return &SQLUserRepository{}
+func NewSQLUserRepository(DB *sqlc.Queries) UserRepository {
+	return &SQLUserRepository{
+		db: DB,
+	}
 }
 
 func (ur *SQLUserRepository) Create(user *models.User) {
