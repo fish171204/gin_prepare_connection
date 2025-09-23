7d8a9707c2cfc8681ea8a2826a462b3188215ce0
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-23 14:06:53 +0700

    feat(db): connect database using PGX and pgxpool

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -1,13 +1,21 @@
 package db
 
-import "hoc-gin/internal/config"
+import (
+	"fmt"
+	"hoc-gin/internal/config"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 var DB string
 
 func InitDB() error {
 	connStr := config.NewConfig().DNS()
 
-	DB = connStr
+	conf, err := pgxpool.ParseConfig(connStr)
+	if err != nil {
+		return fmt.Errorf("error parsing DB config: %v", err)
+	}
 
 	return nil
 }
