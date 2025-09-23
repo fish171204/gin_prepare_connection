74ef91f0cebaa163d92d34aa8ce99c3421b2bd28
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-09-23 14:10:27 +0700

    feat(db): configure advanced connection pool settings for PGX

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -1,8 +1,11 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"hoc-gin/internal/config"
+	"log"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -16,6 +19,26 @@ func InitDB() error {
 	if err != nil {
 		return fmt.Errorf("error parsing DB config: %v", err)
 	}
+
+	conf.MaxConns = 50
+	conf.MinConns = 5
+	conf.MaxConnLifetime = 30 * time.Minute
+	conf.MaxConnIdleTime = 5 * time.Minute
+	conf.HealthCheckPeriod = 1 * time.Minute
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	DBPool, err := pgxpool.NewWithConfig(ctx, conf)
+	if err != nil {
+		return fmt.Errorf("error creating DB pool: %v", err)
+	}
+
+	if err := DBPool.Ping(ctx); err != nil {
+		return fmt.Errorf("db ping error: %v", err)
+	}
+
+	log.Println("Connected")
 
 	return nil
 }
